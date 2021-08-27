bdbc3a1684d107ff89738108f272f3297686f2d7
Aditi Garg <aditig@zg09751.attlocal.net>
2021-08-26 20:39:12 -0700

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Header } from './components/Header';
+import { Cards } from './components/Cards';
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
 
-function App() {
+import { Container, Typography } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    body: {
+      backgroundColor: '#FEF6D8',
+      // background: '#FEF6D8 url(\'/banana-tp-rs.png\') right repeat'
+    },
+  },
+  heroTitle: {
+    color: '#875811',
+    flexGrow: 1,
+    fontWeight: '400',
+  },
+  heroContent: {
+    color: '#875811',
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Header />
+      <Container p={2} maxWidth="sm" component="main" className={classes.heroContent}>
+        <Typography component="h1" variant="h2" align="center" className={classes.heroTitle} gutterBottom>
+          MonkeDAO is a curated community of monkes        
+        </Typography>
+        <Typography variant="h5" align="center" className={classes.heroContent} component="p" gutterBottom>
+            Within the community, everyone is family. We share alpha, jokes, bullshit like there's no tomorrow but have zero tolerance for hatred. We welcome you to the MonkeDAO family!
+        </Typography>
+        <Typography variant="h5" align="center" className={classes.heroContent} component="p">
+            We welcome you to the MonkeDAO family!
+        </Typography>
+      </Container>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Cards/>
+    </React.Fragment>
   );
-}
+};
 
-export default App;
+export default App
