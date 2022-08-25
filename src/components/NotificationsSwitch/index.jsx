ea3f52f42fd2feb96b56dda41217b647f280f3f7
Jennifer Tran <jennifertran@Jennifers-MacBook-Pro.local>
2022-08-25 16:31:25 -0700

    chore: add notifi

--- src/components/NotificationsSwitch/index.jsx
+++ src/components/NotificationsSwitch/index.jsx
@@ -1,23 +1,30 @@
-import { defaultVariables } from '@dialectlabs/react-ui';
-import clsx from 'clsx';
-import DialectNotifications from '../DialectNotifications';
-import NotificationBox from './NotificationBox';
+import { defaultVariables } from "@dialectlabs/react-ui";
+import clsx from "clsx";
+import DialectNotifications from "../DialectNotifications";
+import NotificationBox from "./NotificationBox";
 
-import useOutsideAlerter from './useOutsideAlerter';
+import useOutsideAlerter from "./useOutsideAlerter";
 
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
-import { Box, IconButton, makeStyles, Typography } from '@material-ui/core';
+import { Box, IconButton, makeStyles, Typography } from "@material-ui/core";
 
-const MODAL_STATE_SELECTION = 'selection';
-const MODAL_STATE_DIALECT = 'dialect';
+const MODAL_STATE_SELECTION = "selection";
+const MODAL_STATE_DIALECT = "dialect";
+const MODAL_STATE_NOTIFI = "notifi";
 
 const notificationSolutions = [
   {
-    channels: ['Wallet', 'Email', 'Text', 'Telegram'],
+    channels: ["Wallet", "Email", "Text", "Telegram"],
     description: `Get announcements delivered directly to your wallet, along with telegram, sms and email.`,
     modalState: MODAL_STATE_DIALECT,
-    name: 'Dialect',
+    name: "Dialect",
+  },
+  {
+    channels: ["Email", "Text", "Telegram", "Notifi Center"],
+    description: `Get notifications for proposals, voting, and results. Add your email address, phone number, and/or Telegram.`,
+    modalState: MODAL_STATE_NOTIFI,
+    name: "notifi",
   },
 ];
 
@@ -34,7 +41,12 @@ export default function NotificationsSwitch() {
   return (
     <div className={classes.wrapper} ref={wrapperRef}>
       {openModal && (
-        <div className={clsx(defaultVariables.dark.modalWrapper, classes.modalWrapper)}>
+        <div
+          className={clsx(
+            defaultVariables.dark.modalWrapper,
+            classes.modalWrapper
+          )}
+        >
           {modalState === MODAL_STATE_SELECTION && (
             <Box className={classes.selectionWrapper}>
               <Typography variant="h5" gutterBottom className={classes.header}>
@@ -52,7 +64,15 @@ export default function NotificationsSwitch() {
             </Box>
           )}
 
-          {modalState === 'dialect' && (
+          {modalState === "dialect" && (
+            <DialectNotifications
+              onModalClose={() => {
+                setOpenModal(false);
+              }}
+              onBackClick={() => setModalState(MODAL_STATE_SELECTION)}
+            />
+          )}
+          {modalState === "notifi" && (
             <DialectNotifications
               onModalClose={() => {
                 setOpenModal(false);
@@ -75,33 +95,32 @@ export default function NotificationsSwitch() {
 }
 
 const useStyles = makeStyles((theme) => ({
-  wrapper: { position: 'relative' },
-  modalWrapper: {
-  },
+  wrapper: { position: "relative" },
+  modalWrapper: {},
   announcementsButton: {
-    color: '#f3efcd !important',
-    backgroundColor: '#164120 !important',
+    color: "#f3efcd !important",
+    backgroundColor: "#164120 !important",
   },
   announcementsLogo: {
     marginRight: 8,
-    width: '15%',
-    filter: 'invert(1)',
+    width: "15%",
+    filter: "invert(1)",
   },
   header: {
-    fontFamily: 'Space Grotesk',
-    fontWeight: '600',
+    fontFamily: "Space Grotesk",
+    fontWeight: "600",
   },
   selectionWrapper: {
-    color: '#f3efcd',
-    fontFamily: 'Space Grotesk',
-    fontWeight: '600',
-    position: 'absolute',
+    color: "#f3efcd",
+    fontFamily: "Space Grotesk",
+    fontWeight: "600",
+    position: "absolute",
     right: 0,
-    backgroundColor: '#164120',
-    padding: '1rem',
+    backgroundColor: "#164120",
+    padding: "1rem",
     borderRadius: 10,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
 }));
